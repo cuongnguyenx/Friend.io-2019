188ee9aab67ba0c1321c881533f7f7bec7b4fae8
Trung Dang <bill081001@gmail.com>
2019-09-14 23:44:18 -0400

    Add functionalities
    Auto location update and nearby search

--- app/src/main/java/com/example/friendio_2019/Database.java
+++ app/src/main/java/com/example/friendio_2019/Database.java
@@ -9,8 +9,8 @@ public class Database {
     private DatabaseReference mGeoDatabase;
 
     public Database() {
-        mUserDatabase = FirebaseDatabase.getInstance().getReference();
-        mGeoDatabase = FirebaseDatabase.getInstance().getReference("/geofire");
+        mUserDatabase = FirebaseDatabase.getInstance().getReference("users");
+        mGeoDatabase = FirebaseDatabase.getInstance().getReference("geofire");
     }
 
     public DatabaseReference getUserDatabaseReference() {
@@ -23,13 +23,13 @@ public class Database {
 
     public void writeNewUser(String userID, User user) {
 
-        mUserDatabase.child("users").child(userID).child("firstname").setValue(user.getFirstName());
-        mUserDatabase.child("users").child(userID).child("lastname").setValue(user.getLastName());
-        mUserDatabase.child("users").child(userID).child("bio").setValue(user.getBio());
-        mUserDatabase.child("users").child(userID).child("interest").setValue(user.getInterest());
-        mUserDatabase.child("users").child(userID).child("age").setValue(user.getAge());
-        mUserDatabase.child("users").child(userID).child("profilepic").setValue(user.getEncodedProfilePicture());
-        mUserDatabase.child("users").child(userID).child("status").setValue(user.isAvail());
+        mUserDatabase.child(userID).child("firstname").setValue(user.getFirstName());
+        mUserDatabase.child(userID).child("lastname").setValue(user.getLastName());
+        mUserDatabase.child(userID).child("bio").setValue(user.getBio());
+        mUserDatabase.child(userID).child("interest").setValue(user.getInterest());
+        mUserDatabase.child(userID).child("age").setValue(user.getAge());
+        mUserDatabase.child(userID).child("profilepic").setValue(user.getEncodedProfilePicture());
+        mUserDatabase.child(userID).child("status").setValue(user.isAvail());
 
     }
 }
--- app/src/main/java/com/example/friendio_2019/LocationActivity.java
+++ app/src/main/java/com/example/friendio_2019/LocationActivity.java
@@ -8,13 +8,15 @@ import android.os.Bundle;
 
 import com.firebase.geofire.GeoFire;
 import com.firebase.geofire.GeoLocation;
+import com.firebase.geofire.GeoQuery;
+import com.firebase.geofire.GeoQueryEventListener;
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
@@ -23,111 +25,42 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
-import android.view.View;
-import android.widget.Button;
+import android.os.Looper;
 
 public class LocationActivity extends AppCompatActivity {
 
     private static final int LOCATION_PERMISSION = 1;
     private FusedLocationProviderClient fusedLocationClient;
-    private Location currentLocation;
+    private LocationRequest locationRequest;
+    private LocationCallback locationCallback;
+    private GeoFire geoFire;
+    private GeoQuery geoQuery;
+    private String userID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("geofire");
-        final GeoFire geoFire = new GeoFire(ref);
-        final String userID = "omegalul";
-        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location);
         Toolbar toolbar = findViewById(R.id.toolbar);
-        Button updateButton = findViewById(R.id.updateButton);
         setSupportActionBar(toolbar);
 
-        updateButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                fetchLocation();
-                Double latitude = 0.0;
-                Double longitude = 0.0;
-                if (currentLocation != null) {
-                    latitude = currentLocation.getLatitude();
-                    longitude = currentLocation.getLongitude();
-                }
-                new AlertDialog.Builder(LocationActivity.this)
-                        .setTitle("Please gib location permission uwu")
-                        .setMessage(latitude + ", " + longitude)
-                        .setNegativeButton("OWONO", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                dialog.dismiss();
-                            }
-                        })
-                        .create()
-                        .show();
-                geoFire.setLocation(userID, new GeoLocation(latitude, longitude), new GeoFire.CompletionListener() {
-                    @Override
-                    public void onComplete(String key, DatabaseError error) {
-                        if (error != null) {
-                            new AlertDialog.Builder(LocationActivity.this)
-                                    .setTitle("Please gib location permission uwu")
-                                    .setMessage("Please gib the permission or else this app can't run o//o.")
-                                    .setPositiveButton("OWOK", new DialogInterface.OnClickListener() {
-                                        @Override
-                                        public void onClick(DialogInterface dialog, int which) {
-                                            ActivityCompat.requestPermissions(LocationActivity.this,
-                                                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-                                                    LOCATION_PERMISSION);
-                                        }
-                                    })
-                                    .setNegativeButton("OWONO", new DialogInterface.OnClickListener() {
-                                        @Override
-                                        public void onClick(DialogInterface dialog, int which) {
-                                            dialog.dismiss();
-                                        }
-                                    })
-                                    .create()
-                                    .show();
-                        } else {
-                            System.out.println("Location saved on server successfully!");
-                        }
-                    }
-                });
-            }
-        });
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        if (requestCode == LOCATION_PERMISSION) {
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-
-            } else {
-
-            }
-        }
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-    }
-
-    private void fetchLocation() {
-        System.out.println("something");
+        Database localDatabase = new Database();
+        DatabaseReference ref = localDatabase.getGeoDatabaseReference();
+        geoFire = new GeoFire(ref);
+        userID = "omegalul";
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
 
         if (ContextCompat.checkSelfPermission(LocationActivity.this,
                 Manifest.permission.ACCESS_FINE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED) {
 
-            // Permission is not granted
-            // Should we show an explanation?
             if (ActivityCompat.shouldShowRequestPermissionRationale(LocationActivity.this,
                     Manifest.permission.ACCESS_FINE_LOCATION)) {
-                // Show an explanation to the user *asynchronously* -- don't block
-                // this thread waiting for the user's response! After the user
-                // sees the explanation, try again to request the permission.
 
                 new AlertDialog.Builder(this)
-                        .setTitle("Please gib location permission uwu")
-                        .setMessage("Please gib the permission or else this app can't run o//o.")
-                        .setPositiveButton("OWOK", new DialogInterface.OnClickListener() {
+                        .setTitle("Location permission required")
+                        .setMessage("Please allow location permission for usability.")
+                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialog, int which) {
                                 ActivityCompat.requestPermissions(LocationActivity.this,
@@ -135,30 +68,115 @@ public class LocationActivity extends AppCompatActivity {
                                         LOCATION_PERMISSION);
                             }
                         })
-                        .setNegativeButton("OWONO", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                dialog.dismiss();
-                            }
-                        })
                         .create()
                         .show();
             } else {
-                // No explanation needed; request the permission
                 ActivityCompat.requestPermissions(LocationActivity.this,
                         new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
                         LOCATION_PERMISSION);
             }
         } else {
-            fusedLocationClient.getLastLocation()
-                    .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+            updateLocation();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case LOCATION_PERMISSION: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    updateLocation();
+                } else {
+                    new AlertDialog.Builder(this)
+                            .setTitle("Shutting down ...")
+                            .setMessage("Location permission isn't granted.")
+                            .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    finish();
+                                }
+                            })
+                            .create()
+                            .show();
+                }
+            }
+        }
+    }
+
+    private void updateLocation() {
+        locationRequest = LocationRequest.create();
+        locationRequest.setInterval(10000);
+        locationRequest.setFastestInterval(5000);
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        geoQuery = geoFire.queryAtLocation(new GeoLocation(0.0, 0.0), 0.009144); // 30 feet
+        geoQuery.addGeoQueryEventListener(new GeoQueryEventListener() {
+            @Override
+            public void onKeyEntered(String key, GeoLocation location) {
+                System.out.println(String.format("Key %s entered the search area at [%f,%f]", key, location.latitude, location.longitude));
+            }
+
+            @Override
+            public void onKeyExited(String key) {
+                System.out.println(String.format("Key %s is no longer in the search area", key));
+            }
+
+            @Override
+            public void onKeyMoved(String key, GeoLocation location) {
+                System.out.println(String.format("Key %s moved within the search area to [%f,%f]", key, location.latitude, location.longitude));
+            }
+
+            @Override
+            public void onGeoQueryReady() {
+                System.out.println("All initial data has been loaded and events have been fired!");
+            }
+
+            @Override
+            public void onGeoQueryError(DatabaseError error) {
+                System.err.println("There was an error with this query: " + error);
+            }
+        });
+
+        locationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                if (locationResult == null) {
+                    return;
+                }
+                for (Location location : locationResult.getLocations()) {
+                    if (location == null) {
+                        continue;
+                    }
+                    GeoLocation geoLocation = new GeoLocation(location.getLatitude(), location.getLongitude());
+                    new AlertDialog.Builder(LocationActivity.this)
+                            .setTitle("Current location")
+                            .setMessage(geoLocation.latitude + " " + geoLocation.longitude)
+                            .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    dialog.dismiss();
+                                }
+                            })
+                            .create()
+                            .show();
+
+                    geoFire.setLocation(userID, geoLocation, new GeoFire.CompletionListener() {
                         @Override
-                        public void onSuccess(Location location) {
-                            if (location != null) {
-                                currentLocation = location;
+                        public void onComplete(String key, DatabaseError error) {
+                            if (error != null) {
+                                System.out.println("Location saved unsuccessfully");
+                            } else {
+                                System.out.println("Location saved on server successfully!");
                             }
                         }
                     });
-        }
+                    geoQuery.setCenter(geoLocation);
+                }
+            }
+        };
+
+        fusedLocationClient.requestLocationUpdates(locationRequest,
+                locationCallback,
+                Looper.getMainLooper());
     }
 }
